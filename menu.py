470f2fe65315a444a4503eaf1530fc2c6f3d5806
denisbarbu <barbudenisphoto@gmail.com>
2024-07-28 11:47:18 -0700

    Commit Message

---
+++ menu.py
@@ -0,0 +1,9 @@
+import pygame
+
+SCREEN_HEIGHT = 1080
+SCREEN_WIDTH = 1080
+
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+pygame.display.set_caption("SpaceWar2")
+
+start_img = pygame.image.load("assets/start")
